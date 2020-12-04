fffeb9700bf22bce2920a05867ae5ac35a7a5571
hungkunfu <31463861+hungkunfu@users.noreply.github.com>
2020-12-04 16:20:45 +0700

    date time

---
+++ Week1/Hien_Thi_Thoi_Gian_HT/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Hien_Thi_Thoi_Gian_HT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           DateTime locadate = DateTime.Now;
+           System.Console.WriteLine("=============time he thong ===============");
+           System.Console.WriteLine("Date now is"+ " : " + locadate);
+           System.Console.WriteLine("==============ban da thanh cong==========");
+        }
+    }
+}
