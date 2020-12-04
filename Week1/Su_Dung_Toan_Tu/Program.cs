fbf5f92b8c4649e2c69941407f5a4ff66c9785e4
hungkunfu <31463861+hungkunfu@users.noreply.github.com>
2020-12-04 16:38:21 +0700

    Su Dung Toan Tu

---
+++ Week1/Su_Dung_Toan_Tu/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Su_Dung_Toan_Tu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          float hight;
+          float width;
+         System.Console.WriteLine("Nhap vao hight:");
+         hight = float.Parse(Console.ReadLine());
+         System.Console.WriteLine("Nhap vao width:");
+         width = float.Parse(Console.ReadLine());
+         float area = width * hight;
+         System.Console.WriteLine("area = " + area);
+        }
+    }
+}
