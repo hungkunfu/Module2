7cfa046dacaf1c3e7a05015a47db3d33632b965e
hungkunfu <31463861+hungkunfu@users.noreply.github.com>
2020-12-04 15:16:48 +0700

    module 2

---
+++ Week1/month1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static double BinhPhuong(double number)
+        {
+            double ketqua = number * number;
+            return ketqua;
+
+        }
+        static void Main(string[] args)
+        {
+            // Console.InputEncoding.UTF8;
+            // Console.OutputEncoding = Encoding.UTF8;
+            double tong = BinhPhuong(6);
+            Console.WriteLine("======= chương trình thử nghiệm đầu tiên =======");
+            Console.WriteLine($"tích là " + tong);
+            Console.WriteLine("Bạn đã thành công");
+            Console.ReadKey();
+        }
+
+
+
+    }
+}
