de19c233b3f432aa81a93e8520a7cb77ea301519
hungkunfu <31463861+hungkunfu@users.noreply.github.com>
2020-12-22 14:53:18 +0700

    Thuc Hanh

---
+++ TH_Stack_Use_Arraylist/TH_Stack_Use_Arraylist/MyStack.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TH_Stack_Use_Arraylist
+{
+    class MyStack
+    {
+        private int[] arr;
+        private int size;
+        private int index = 0;
+        public MyStack(int size)
+        {
+            this.size = size;
+            arr = new int[size];
+
+        }
+        public bool isEmpty()
+        {
+            if(index == 0)
+            {
+                return false;
+            }
+            return true;
+        }
+        public bool isFull()
+        {
+            if(index == size)
+            {
+                return true;
+            }
+            return false;
+        }
+        public int Size()
+        {
+            return index;
+        }
+        public void Push(int element)
+        {
+            if (isFull())
+            {
+                throw new Exception("Stack IS full");
+            }
+            arr[index] = element;
+            index ++;
+        }
+        public int Pop()
+        {
+            if (isEmpty())
+            {
+                throw new Exception("Stack IS null");
+            }
+            return arr[--index];
+        }
+
+    }
+}
---
+++ TH_Stack_Use_Arraylist/TH_Stack_Use_Arraylist/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TH_Stack_Use_Arraylist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack stack = new MyStack(10);
+
+            stack.Push(5);
+            stack.Push(4);
+            stack.Push(3);
+            stack.Push(2);
+            stack.Push(1);
+            stack.Push(7);
+            stack.Push(8);
+            Console.WriteLine("1. Size of stack after push operations: " + stack.Size());
+            Console.WriteLine("2. Pop elements from stack : ");
+
+            while (!stack.isEmpty())
+            {
+                Console.WriteLine(stack.Pop());
+            }
+
+            Console.WriteLine("\n3. Size of stack after pop operations : " + stack.Size());
+            Console.ReadKey();
+        }
+    }
+}
