2fe450f295903a19a5239b5a52862f4b21b44a02
hungkunfu <31463861+hungkunfu@users.noreply.github.com>
2020-12-23 23:17:01 +0700

    Thuc Hanh remove _ Add _ Repair

---
+++ TH_Remove_Add_Repair/TH_Remove_Add_Repair/Book.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TH_Remove_Add_Repair
+{
+    class Book
+    {
+        private string _nxb;
+
+        public string Nxb
+        {
+            get { return _nxb; }
+            set { _nxb = value; }
+        }
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        private string _author;
+
+        public string Author
+        {
+            get { return _author; }
+            set { _author = value; }
+        }
+        public string ViewInfo()
+        {
+            return $"{_nxb}\t\t\t{_name}\t\t\t{_author}";
+        }
+        public void PrintBook(Dictionary<string, Book> books)
+        {
+            foreach (KeyValuePair<string, Book> bookl in books)
+            {
+                //Book book = bookl.Value;
+                Console.WriteLine(bookl.Value.ViewInfo());
+            }
+
+        } 
+
+
+
+    }
+}
---
+++ TH_Remove_Add_Repair/TH_Remove_Add_Repair/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TH_Remove_Add_Repair
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book();
+            Dictionary<string, Book> bookList = new Dictionary<string, Book>();
+            bookList.Add("9780321344755", new Book { Nxb = "9780321344755", Name = "Don't Make Me Think (Beta)", Author = "Steve Krug" });
+            bookList.Add("1491904909", new Book { Nxb = "1491904909", Name = "User Story Mapping", Author = "Jeff Patton" });
+            bookList.Add("9780465050659", new Book { Nxb = "9780465050659", Name = "The Design of Everyday Things", Author = "Don Norman" });
+            book.PrintBook(bookList);
+
+            // sua name of booklist 
+            bookList["9780321344755"] = new Book { Nxb = "9780321344755", Name = "Don't Make Me Think", Author = "Steve Krug" };
+
+            Console.WriteLine("Edit book with ISBN");
+            book.PrintBook(bookList);
+            // search phan tu trong booklist
+            Console.WriteLine("Search for a specific book with ISBN");
+            if (bookList.ContainsKey("9780321344755") == false)
+            {
+                Console.WriteLine("Not found");
+            }
+            else
+            {
+                Book books = bookList["9780321344755"];
+                Console.WriteLine("Found this book: " + books.Name);
+            }
+            // remove mootj phan tu cua booklisst
+            bookList.Remove("9780321344755");
+
+            Console.WriteLine("Remove book with ISBN");
+            book.PrintBook(bookList);
+        }
+    }
+}
