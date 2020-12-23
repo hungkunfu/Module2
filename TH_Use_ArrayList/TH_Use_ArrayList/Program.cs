6d6ed20eedc45966df02c7e7b80cc9b7f1d6e6a4
hungkunfu <31463861+hungkunfu@users.noreply.github.com>
2020-12-23 22:57:27 +0700

    Thuc hanh arraylist

---
+++ TH_Use_ArrayList/TH_Use_ArrayList/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+
+namespace TH_Use_ArrayList
+{
+    class Program
+    {
+       public static ArrayList studenlist = new ArrayList(); 
+        static void Main(string[] args)
+        {
+            studenlist.Add(new Student { Name = "Hung tran", Age = 20 });
+            studenlist.Add(new Student { Name = "Dieu Hanh", Age = 22 });
+            studenlist.Add(new Student { Name = "le tran", Age = 20 });
+            studenlist.Add(new Student { Name = "Dieu h", Age = 22 });
+            studenlist.Add(new Student { Name = "Hung hung", Age = 24 });
+            studenlist.Add(new Student { Name = "hanh Hanh", Age = 19 });
+            foreach (Student item in studenlist)
+            {
+                Console.WriteLine(item.ViewInfo());
+            }
+            studenlist.RemoveAt(3);
+            Console.WriteLine();
+            foreach (Student item in studenlist)
+            {
+                Console.WriteLine(item.ViewInfo());
+            }
+        }
+    }
+}
---
+++ TH_Use_ArrayList/TH_Use_ArrayList/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TH_Use_ArrayList
+{
+    class Student
+    {
+        public string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public int _age;
+
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+        public Student() { }
+        public Student(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+        public string ViewInfo()
+        {
+            return $"{_name}\t\t\t{_age}";
+        }
+
+
+    }
+}
