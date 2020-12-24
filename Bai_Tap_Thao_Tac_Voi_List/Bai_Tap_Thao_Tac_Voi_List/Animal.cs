ba0a9d596a7a8240fb5be3c5e7145febd86b4b29
hungkunfu <31463861+hungkunfu@users.noreply.github.com>
2020-12-24 11:27:21 +0700

    Bai tap Thao tac voi arraylist

---
+++ Bai_Tap_Thao_Tac_Voi_List/Bai_Tap_Thao_Tac_Voi_List/Animal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace Bai_Tap_Thao_Tac_Voi_List
+{
+    class Animal
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public Animal(string name) {
+            Name = name;
+        }
+        
+
+    }
+    class SortPersonByName : IComparer<Animal>
+    {
+       
+
+        public int Compare([AllowNull] Animal x, [AllowNull] Animal y)
+        {
+            return x.Name.CompareTo(y.Name);
+        }
+    }
+
+}
---
+++ Bai_Tap_Thao_Tac_Voi_List/Bai_Tap_Thao_Tac_Voi_List/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Bai_Tap_Thao_Tac_Voi_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Animal> animalList = new List<Animal>()
+            {
+                new Animal("lion"),
+                new Animal("cat"),
+                new Animal("dog"),
+                new Animal("elephant")
+            };
+           
+
+            Console.WriteLine("Danh sach Chua xoa");
+            foreach (var item in animalList)
+            {
+                Console.WriteLine(item.Name);
+            }
+
+            Console.WriteLine("Danh sach da xoa dog \n");
+            animalList.RemoveAt(2);
+            foreach (var item in animalList)
+            {
+                Console.WriteLine(item.Name);
+            }
+            Console.WriteLine("Danh sach Sap sau khi sap sep \n");
+            animalList.Sort(new SortPersonByName());
+            foreach (var item in animalList)
+            {
+                Console.WriteLine(item.Name);
+            }
+            Console.ReadKey();
+        }
+
+    }
+   
+
+}
