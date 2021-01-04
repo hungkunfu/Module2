1f90fbf02f7958d35dc55bad9cae5b7129483740
hungkunfu <31463861+hungkunfu@users.noreply.github.com>
2021-01-04 09:40:03 +0700

    Bai tap

---
+++ Bai_Tap_Khoi_Tao_Generic/Bai_Tap_Khoi_Tao_Generic/GenericList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bai_Tap_Khoi_Tao_Generic
+{
+    class GenericList
+    {
+        public bool compares<T>( T a,  T b)
+        {
+           
+            if (a.Equals(b))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+
+            }
+    
+        }
+    }
+}
---
+++ Bai_Tap_Khoi_Tao_Generic/Bai_Tap_Khoi_Tao_Generic/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Bai_Tap_Khoi_Tao_Generic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList list = new GenericList();
+            int a = 6;
+            int b = 6;
+            list.compares<int>( a,   b);
+        
+            if (list.compares<int>( a,  b))
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+
+            string sr = "hung tran";
+            string st = "Dieu Hanh";
+            
+            if (list.compares<string>(sr, st))
+            {
+                Console.WriteLine("true");
+            }else
+            {
+                Console.WriteLine("False");
+            }
+            Console.ReadKey();
+        }
+    }
+}
